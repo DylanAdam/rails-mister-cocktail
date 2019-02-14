388eb63da0625f4eab5c2c6d6eb48f50616a765c
DylanAdam <adamdylan.p@gmail.com>
2019-02-14 17:42:53 +0100

    added some css

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,40 @@
+class CocktailsController < ApplicationController
+	before_action :set_cocktail, only: [:show, :edit]
+
+  def index
+		@cocktails = Cocktail.all
+  end
+
+  def show
+    @doses = @cocktail.doses
+    
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def edit
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:cocktail).permit(:name, :doses, :ingredients)
+  end
+end
